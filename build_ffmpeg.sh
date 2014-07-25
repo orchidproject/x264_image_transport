00246a7ee3c8e4daf3be3b87be4b14ba15a3b8c1
Botev <botevmg@gmail.com>
2014-07-25 14:56:03 +0100

    Fixed it to be like in the official guide, seems to work

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -1,17 +1,29 @@
 mkdir -p ~/ffmpeg_sources
 cd ~/ffmpeg_sources
-wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
-tar -zxvf lame-3.99.5.tar.gz
-cd lame-3.99.5 
-PATH="$PATH:$HOME/bin" ./configure
-PATH="$PATH:$HOME/bin" make
-make install
 wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
 tar xjvf last_x264.tar.bz2
 cd x264-snapshot*
 PATH="$PATH:$HOME/bin" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-shared
 PATH="$PATH:$HOME/bin" make
 make install
+make distclean
+sudo apt-get install nasm
+cd ~/ffmpeg_sources
+wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+tar xzvf lame-3.99.5.tar.gz
+cd lame-3.99.5
+./configure --prefix="$HOME/ffmpeg_build" --enable-nasm --disable-shared
+make
+make install
+make distclean
+cd ~/ffmpeg_sources
+wget http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
+tar xzvf opus-1.1.tar.gz
+cd opus-1.1
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+make distclean
 sudo apt-get install unzip
 cd ~/ffmpeg_sources
 wget -O fdk-aac.zip https://github.com/mstorsjo/fdk-aac/zipball/master
@@ -48,19 +60,19 @@ PATH="$PATH:$HOME/bin" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
   --bindir="$HOME/bin" \
   --extra-libs="-ldl" \
-  --enable-pic \
   --enable-gpl \
   --enable-libass \
   --enable-libfdk-aac \
   --enable-libfreetype \
   --enable-libmp3lame \
+  --enable-libopus \
   --enable-libtheora \
   --enable-libvorbis \
   --enable-libvpx \
-  --enable-shared \
   --enable-libx264 \
   --enable-nonfree \
-  --enable-x11grab
+  --enable-x11grab \
+  --enable-pic
 PATH="$PATH:$HOME/bin" make
 make install
 make distclean
